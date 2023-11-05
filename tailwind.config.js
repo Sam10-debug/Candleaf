4e27e3be85c902a9266ba736cb4a668d140af8ef
Samuel <emorinkensam@gmail.com>
2023-11-05 18:06:38 +0100

    f

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: {
+        'sm': '650px',
+        // Other screen sizes...
+      },
       colors:{
         buttonColor:'#56B280',
         blackTexts:'#1D252C',
